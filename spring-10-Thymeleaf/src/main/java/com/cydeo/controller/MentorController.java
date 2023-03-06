fc5fa7f1287e83331b4deed89b4f9e0e78bd32e8
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-03-06 14:22:27 -0500

    commit Thymeleaf module

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -1,7 +1,14 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
 
 @Controller
 @RequestMapping("/mentor")
@@ -15,14 +22,23 @@ public class MentorController {
 //        return "student/register";
 //    }
 
+    @GetMapping("/register") //localhost:8080/mentor/register
+    public String register(Model model){
+
+        List<String> batchList = Arrays.asList("JD1","JD2","JD3");
+        model.addAttribute("batchList",batchList);
+        model.addAttribute("mentor",new Mentor());
+
+        return "mentor/mentor-register";
+    }
+
     @RequestMapping("/register") // localhost:8080/mentor/register
-    public String register(){
+    public String register() {
         return "student/register";
     }
+
     @RequestMapping("/drop") // localhost:8080/mentor/drop
-    public String drop(){
+    public String drop() {
         return "student/register";
     }
-
-
 }
