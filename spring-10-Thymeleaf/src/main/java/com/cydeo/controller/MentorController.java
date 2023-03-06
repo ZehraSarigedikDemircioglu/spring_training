815fe31cf8ec025fcdb76c5db1359b2797ccd398
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-03-06 15:56:18 -0500

    commit Thymeleaf module

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -4,6 +4,8 @@ import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -41,4 +43,30 @@ public class MentorController {
     public String drop() {
         return "student/register";
     }
+
+//    @PostMapping("/confirm")
+//    public String submitForm(@ModelAttribute("mentor") Mentor mentor, Model model){
+//
+//        how can I access to "mentor" attribute in this method? It comes @ModelAttribute("mentor")
+//        model.addAttribute("mentor",new Mentor());
+//        return "mentor/mentor-register";
+//    }
+
+    @PostMapping("/confirm")
+    public String submitForm(@ModelAttribute("mentor") Mentor mentor){
+
+//        return "mentor/mentor-register";
+//        no need repeated codes, it comes redirect:
+//        each completed registration, page will refresh with empty form and gives me register
+        return "redirect:/mentor/register";
+    }
+
+
+
+//    @GetMapping("/confirm")
+//    // it is possible an end-point works both get and post-mapping
+//    public String submitForm2(){
+//
+//        return "mentor/mentor-confirmation";
+//    }
 }
