7d9501edd3788fa53d1312c77c95cb81e23444d5
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-04 12:32:03 -0400

    added an entity

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
@@ -1,7 +1,11 @@
 package com.cydeo.entity;
 
-public class Student {
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class Student {
+    @Id
     private Long id;
     private String firstName;
     private String lastName;
