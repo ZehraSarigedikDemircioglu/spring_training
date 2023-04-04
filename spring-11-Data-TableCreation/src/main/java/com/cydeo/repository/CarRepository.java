00beb66e1ce925fa5c4170871d532e36dcca4f4c
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-04 15:05:21 -0400

    edited car repository

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/repository/CarRepository.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/repository/CarRepository.java
@@ -2,6 +2,8 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+//@Repository // Spring boot creates impl for us. So, we put annotation interface level. After some version, this is not needed. We can ignore this annotation too.
 public interface CarRepository extends JpaRepository<Car, Long> {
 }
