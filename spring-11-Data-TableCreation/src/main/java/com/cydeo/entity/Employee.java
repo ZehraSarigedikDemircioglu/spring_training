6e21670c414658fe153cbfa0996725036ca3e196
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-04 15:48:06 -0400

    commit base entity

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Employee.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Employee.java
@@ -1,11 +1,24 @@
 package com.cydeo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import com.cydeo.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
-public class Employee {
-    @Id
-    private int id;
-    private String name;
+@Table(name = "employees")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee extends BaseEntity{
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    @Column(columnDefinition = "DATE")
+    private LocalDate hireDate;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    private Integer salary;
 }
