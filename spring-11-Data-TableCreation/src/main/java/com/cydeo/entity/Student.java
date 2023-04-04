3097c43851edeed495501c9b8933a468f90675c8
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-04 12:50:45 -0400

    updated

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
@@ -1,11 +1,12 @@
 package com.cydeo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "students") // this is the good practice with making plural form
 public class Student {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // postgres is creating
     private Long id;
     private String firstName;
     private String lastName;
