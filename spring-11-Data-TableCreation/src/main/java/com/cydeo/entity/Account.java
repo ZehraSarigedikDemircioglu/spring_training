462c7247aea56dcd861f871ede305c7e993c4421
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-04 14:02:52 -0400

    updated account class

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Account.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Account.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import java.math.BigDecimal;
 
 @MappedSuperclass // it won't create account table, using this annotation allow an entity to inherit properties from a base class
+// so, we have credit account table and debit account table as child classes of account class
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
