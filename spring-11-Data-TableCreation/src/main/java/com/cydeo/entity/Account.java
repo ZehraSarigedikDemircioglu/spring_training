818f86f6cb84d5e5cf08f5020c610f083533e841
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-04 14:01:41 -0400

    created account class

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Account.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Account.java
@@ -3,7 +3,7 @@ package com.cydeo.entity;
 import javax.persistence.*;
 import java.math.BigDecimal;
 
-@MappedSuperclass
+@MappedSuperclass // it won't create account table, using this annotation allow an entity to inherit properties from a base class
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
