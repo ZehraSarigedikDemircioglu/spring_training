0b34bce74d03e95826a44b8736fce96273f29ffa
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-04 12:31:39 -0400

    updated

--- spring-11-Data-TableCreation/src/main/resources/data.sql
+++ spring-11-Data-TableCreation/src/main/resources/data.sql
@@ -1,2 +1,8 @@
 INSERT INTO EMPLOYEES(id, name)
 VALUES(1, 'Mike');
+
+INSERT INTO student(id, first_name, last_name, email)
+VALUES(1, 'Mike', 'Smith', 'm.smith@gmail.com');
+
+iNSERT INTO student(id, first_name, last_name, email)
+VALUES(2, 'Tom', 'Hanks', 'tom.hank@gmail.com');
