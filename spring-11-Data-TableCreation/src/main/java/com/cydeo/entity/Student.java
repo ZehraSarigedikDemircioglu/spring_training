52fae5494251f3e379e9820954b11df0b8518ff2
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-04 13:10:12 -0400

    updated

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
@@ -1,6 +1,9 @@
 package com.cydeo.entity;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Entity
 @Table(name = "students") // this is the good practice with making plural form
@@ -8,8 +11,17 @@ public class Student {
     @Id // whatever we write any annotation this section, it is a sql code behind of scene.
     @GeneratedValue(strategy = GenerationType.IDENTITY) // postgres is creating
     private Long id;
+
+    @Column(name = "studentFirstName") // it is not common
     private String firstName;
+    @Column(name = "studentLastName")
     private String lastName;
     private String email;
+    @Column(columnDefinition = "DATE")
+    private LocalDate birthDate;
+    @Column(columnDefinition = "TIME")
+    private LocalTime birthTime;
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime birthDateTime;
 
 }
