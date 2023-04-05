cace1f3c03857f93efc753ffab039e84b79ce149
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-05 16:14:26 -0400

    updated merchant

--- spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Merchant.java
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Merchant.java
@@ -21,7 +21,7 @@ public class Merchant {
     private BigDecimal transactionFee;
     private BigDecimal commissionRate;
     private Integer payoutDelayCount;
-    @OneToMany // //in OneToMany relationship, ownership belongs to many side
+    @OneToMany(mappedBy = "merchant") // //in OneToMany relationship, ownership belongs to many side
     private List<Payment> paymentList;
 
     public Merchant(String name, String code, BigDecimal transactionFee, BigDecimal commissionRate, Integer payoutDelayCount) {
