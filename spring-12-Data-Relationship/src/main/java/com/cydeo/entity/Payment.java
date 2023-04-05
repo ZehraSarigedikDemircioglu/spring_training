a4a829fa07726658bf1752e9991707e1463e42f5
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-05 13:40:41 -0400

    edited payment

--- spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Payment.java
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Payment.java
@@ -22,6 +22,7 @@ public class Payment {
     @Enumerated(EnumType.STRING)
     private Status paymentStatus;
     @OneToOne
+    // @JoinColumn(name = "payment_detail_id") change the name of foreign key, but optional since already provided automatically
     private PaymentDetail paymentDetail;
 
     public Payment(LocalDate createdDate, BigDecimal amount, Status paymentStatus) {
