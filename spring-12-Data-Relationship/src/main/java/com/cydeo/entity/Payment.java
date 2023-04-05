8fbfc921e555602c82405537f5fc9fa65bc0f244
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-05 13:06:12 -0400

    edited

--- spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Payment.java
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Payment.java
@@ -1,5 +1,6 @@
 package com.cydeo.entity;
 
+import com.cydeo.enums.Status;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -18,5 +19,12 @@ public class Payment {
     @Column(columnDefinition = "DATE")
     private LocalDate createdDate;
     private BigDecimal amount;
+    @Enumerated(EnumType.STRING)
+    private Status paymentStatus;
 
+    public Payment(LocalDate createdDate, BigDecimal amount, Status paymentStatus) {
+        this.createdDate = createdDate;
+        this.amount = amount;
+        this.paymentStatus = paymentStatus;
+    }
 }
