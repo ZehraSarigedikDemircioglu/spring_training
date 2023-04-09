b1833c198d70d70eac3e606174675a8fc076728e
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-09 02:22:34 -0400

    entity commit

---
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Cinema.java
@@ -0,0 +1,20 @@
+package com.cydeo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Cinema {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String name;
+    private String sponsoredName;
+    @ManyToOne
+    private Location location;
+}
