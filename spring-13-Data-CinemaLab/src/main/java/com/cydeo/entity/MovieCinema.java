7609348074656b16d1ed3eb144a5c74605892175
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-10 10:51:59 -0400

    entity added

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/MovieCinema.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/MovieCinema.java
@@ -1,20 +1,18 @@
 package com.cydeo.entity;
 
-import lombok.Data;
+
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @NoArgsConstructor
-@Data
-
-public class MovieCinema {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+@Getter
+@Setter
+public class MovieCinema extends BaseEntity{
     @Column(columnDefinition = "DATE")
     private LocalDate dateTime;
     @ManyToOne
