9c428d6bc2d76a43f2d6ade79e0109b4f0f14853
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-10 15:45:31 -0400

    entity added

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Location.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Location.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import java.math.BigDecimal;
 
 
 @Entity
@@ -15,8 +16,8 @@ import javax.persistence.Entity;
 public class Location extends BaseEntity{
 
     private String name;
-    private String  latitude;
-    private String longitude;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
     private String country;
     private String city;
     private String state;
