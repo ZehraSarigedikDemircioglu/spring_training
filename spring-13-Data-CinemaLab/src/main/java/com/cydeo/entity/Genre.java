7ca59a702962974d8672c0d3b3c80ee78b97d14d
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-10 10:52:52 -0400

    entity added

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Genre.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Genre.java
@@ -1,18 +1,18 @@
 package com.cydeo.entity;
 
-import lombok.Data;
+
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.List;
+
 @Entity
 @NoArgsConstructor
-@Data
-public class Genre {
+@Getter
+@Setter
+public class Genre extends BaseEntity{
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
     private String name;
 
 }
