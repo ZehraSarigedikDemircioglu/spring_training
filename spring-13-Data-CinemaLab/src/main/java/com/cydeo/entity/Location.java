babf4dc7bf1342007d24daa9f6304c8d713ddc27
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-10 10:52:32 -0400

    entity added

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Location.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Location.java
@@ -1,26 +1,24 @@
 package com.cydeo.entity;
 
-import lombok.Data;
+
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
 @Entity
 @NoArgsConstructor
-@Data
-public class Location {
+@Getter
+@Setter
+public class Location extends BaseEntity{
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
     private String name;
-    private String address;
     private String latitude;
     private String longitude;
     private String country;
     private String city;
     private String state;
     private String postalCode;
+    private String address;
 }
