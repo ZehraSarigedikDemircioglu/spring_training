e7f411ec2463a3b903706cf03ab9c10d250180c0
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-10 10:52:59 -0400

    entity added

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Movie.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Movie.java
@@ -2,8 +2,9 @@ package com.cydeo.entity;
 
 import com.cydeo.enums.State;
 import com.cydeo.enums.Type;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -12,22 +13,18 @@ import java.util.List;
 
 @Entity
 @NoArgsConstructor
-@Data
-
-public class Movie {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+@Getter
+@Setter
+public class Movie extends BaseEntity{
 
     private String name;
+    private BigDecimal price;
     @Enumerated(EnumType.STRING)
     private State state;
     @Enumerated(EnumType.STRING)
     private Type type;
     @Column(columnDefinition = "TIMESTAMP")
     private LocalDateTime releaseDate;
-    private BigDecimal price;
     private Integer duration;
     private String summary;
     @ManyToMany
