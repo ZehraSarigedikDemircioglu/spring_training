e881bd34e548d7c081704685a22b4ed023ab1235
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-10 10:53:37 -0400

    repository added

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
@@ -1,4 +1,8 @@
 package com.cydeo.repository;
 
-public interface CinemaRepository {
+
+import com.cydeo.entity.Cinema;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CinemaRepository extends JpaRepository<Cinema, Long> {
 }
