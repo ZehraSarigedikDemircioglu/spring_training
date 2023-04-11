ddbdef730a0db9b3e606e847d2a237a051ce7e2e
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-11 13:49:12 -0400

    entity added

---
+++ spring-14-Data-Queries/src/main/java/com/cydeo/entity/Region.java
@@ -0,0 +1,19 @@
+package com.cydeo.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "regions")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Region extends BaseEntity{
+
+    private String region;
+    private String country;
+}
