35e793fe82b886a0b5c95a2d1d2bb5374e71354a
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-13 01:10:18 -0400

    updated

--- spring-14-Data-Queries/src/main/java/com/cydeo/entity/Course.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/entity/Course.java
@@ -3,7 +3,6 @@ package com.cydeo.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.procedure.spi.ParameterRegistrationImplementor;
 
 import javax.persistence.*;
 
