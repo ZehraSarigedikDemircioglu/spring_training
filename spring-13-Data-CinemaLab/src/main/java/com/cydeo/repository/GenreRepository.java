3b865788418a5391f360722479ffb131a2ca0f43
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-13 01:06:55 -0400

    added queries

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/GenreRepository.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/GenreRepository.java
@@ -2,6 +2,21 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 public interface GenreRepository extends JpaRepository<Genre, Long> {
+
+    // ------------------- JPQL QUERIES ------------------- //
+
+    //Write a JPQL query that return all genres
+    @Query("select g from Genre g")
+    List<Genre> findAll();
+
+
+    // ------------------- Native QUERIES ------------------- //
+
+    //Write a native query that returns genres by containing name
+    @Query(value = "select g from Genre g where name ilike %?1%", nativeQuery = true)
+    List<Genre> findByNameContains(String name);
 }
