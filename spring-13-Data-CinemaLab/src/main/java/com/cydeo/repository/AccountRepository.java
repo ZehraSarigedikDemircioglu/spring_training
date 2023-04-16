68696509ccb3d2c0e2f2ab176017aaae1efbbe53
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-16 12:16:09 -0400

    added queries

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/AccountRepository.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/AccountRepository.java
@@ -4,6 +4,8 @@ import com.cydeo.entity.Account;
 import com.cydeo.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
@@ -11,7 +13,7 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     // ------------------- DERIVED QUERIES ------------------- //
 
     //Write a derived query to list all accounts with a specific country or state
-    List<Account> findAccountByCountryOrState(String country, String state);
+    List<Account> findByCountryOrState(String country, String state);
 
 
     //Write a derived query to list all accounts with age lower than or equal to a specific value
@@ -60,7 +62,7 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
 
     //Write a native query to read all accounts that a specific value can be containable in the name, address, country, state city
     @Query(value = "select * from account_details where name ilike %?1% or address ilike %?1% or country ilike %?1% or state ilike %?1% or city ilike %?1%", nativeQuery = true)
-    List<Account> retrieveAllAccountsContainsNameAddressCountryStateCity(String pattern);
+    List<Account> retrieveAllAccountsContainsNameAddressCountryStateCity(@Param("pattern") String pattern);
 
 
     //Write a native query to read all accounts with an age higher than a specific value
