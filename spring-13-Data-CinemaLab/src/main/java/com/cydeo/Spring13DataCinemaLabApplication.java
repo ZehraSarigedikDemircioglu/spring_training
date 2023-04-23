b30fe1514830361b94ea6b5181042a56a3b0f087
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-23 16:54:04 -0400

    updated

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/Spring13DataCinemaLabApplication.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/Spring13DataCinemaLabApplication.java
@@ -14,9 +14,9 @@ public class Spring13DataCinemaLabApplication {
     public static void main(String[] args) {
         SpringApplication.run(Spring13DataCinemaLabApplication.class, args);
     }
-    	@Bean
-	public MigrateResult migrateResult(DataSource dataSource){
-		return Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
-	}
+//    	@Bean
+//	public MigrateResult migrateResult(DataSource dataSource){
+//		return Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
+//	}
 
 }
