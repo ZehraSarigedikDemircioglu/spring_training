d9aac29c756379f445afecbb58a3ecf420540b5d
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-23 16:52:34 -0400

    added some queries

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/MovieRepository.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/MovieRepository.java
@@ -40,7 +40,7 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     //Write a JPQL query that returns all movie names
     @Query("select m.name from Movie m")
-    List<Movie> retrieveAllMovieNames();
+    List<String> retrieveAllMovieNames();
 
 
     // ------------------- Native QUERIES ------------------- //
