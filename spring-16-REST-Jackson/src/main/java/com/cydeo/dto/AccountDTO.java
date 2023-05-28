0af0e980a47f345a73cfea90bdda1186d8bc6a57
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-28 13:25:05 -0400

    DTO added

---
+++ spring-16-REST-Jackson/src/main/java/com/cydeo/dto/AccountDTO.java
@@ -0,0 +1,28 @@
+package com.cydeo.dto;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(value = {"age", "city"}, ignoreUnknown = true)
+//@JsonIgnoreProperties(ignoreUnknown = true)
+public class AccountDTO {
+
+    private String name;
+    private String address;
+    private String country;
+    private String state;
+    private String city;
+    private Integer age;
+    private String postalCode;
+
+    @JsonBackReference  // This field is not going to be serialized, will hide in the db
+    private UserDTO user;
+
+}
