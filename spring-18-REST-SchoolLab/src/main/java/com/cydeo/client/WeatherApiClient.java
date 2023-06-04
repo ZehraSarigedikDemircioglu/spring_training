70f99eb0d8cb014f5fe9e2eeea19cf25d1359b7c
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-06-04 13:07:23 -0400

    added interface client

--- spring-18-REST-SchoolLab/src/main/java/com/cydeo/client/WeatherApiClient.java
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/client/WeatherApiClient.java
@@ -1,5 +1,6 @@
 package com.cydeo.client;
 
+import com.cydeo.dto.weather.WeatherDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,5 +11,5 @@ public interface WeatherApiClient {
     // http://api.weatherstack.com/current?access_key=87e785a9468054d73eaf989af1e4a99a&query=London
 
     @GetMapping("/current")
-    Object getWeather(@RequestParam(value = "access_key") String key, @RequestParam(value = "query") String location); // Required access key, query
+    WeatherDTO getWeather(@RequestParam(value = "access_key") String key, @RequestParam(value = "query") String city); // Required access key, query
 }
