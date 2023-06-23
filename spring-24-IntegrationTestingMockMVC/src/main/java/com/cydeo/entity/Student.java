014ba3c994469fbf06ea61c12b51b2f3151a8eea
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-06-23 16:49:31 -0400

    added entity

---
+++ spring-24-IntegrationTestingMockMVC/src/main/java/com/cydeo/entity/Student.java
@@ -0,0 +1,28 @@
+package com.cydeo.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Student(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+}
